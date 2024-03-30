289090752986a833976312f4ae0ee3272833b1e5
Rahul Mijar <rahul.mijar@trunary.com>
2024-03-30 17:51:25 +0545

    feat: add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import GlobalStyles from "./styles/GlobalStyle";
+
+// global style component does not except child components so we need to add as a sibling
+// to the main div component
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <h1>This is a first app</h1>
+      </div>
+    </>
+  );
 }
