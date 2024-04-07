c5034facc486ecd22525b64ddffb7884a6c9b16b
Rahul Mijar <rahul.mijar@trunary.com>
2024-04-07 16:18:46 +0545

    fixed: bug on user login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -13,7 +13,7 @@ export default function useLogin() {
       console.log(err);
     },
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
   });
